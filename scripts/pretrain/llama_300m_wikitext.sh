0a992d8ea303973db3e1e3c42fed1bbbd6015192
Maxwell-Jia <mr.minghui.jia@gmail.com>
2024-05-20 01:58:50 +0800

    llama 0.3B config & scrash train

--- scripts/pretrain/llama_300m_wikitext.sh
+++ scripts/pretrain/llama_300m_wikitext.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 python src/exp/run_pretrain.py \
-    --model_name_or_path openai-community/gpt2 \
+    --config_name configs/models/Llama-300M.json \
+    --tokenizer_name nvidia/Llama3-ChatQA-1.5-8B \
     --dataset_name wikitext \
     --dataset_config_name wikitext-2-raw-v1 \
+    --block_size 8192 \
+    --max_train_samples 1000 \
     --per_device_train_batch_size 1 \
     --per_device_eval_batch_size 1 \
     --do_train \
-    --output_dir ./outputs/debug \
-    --overwrite_output_dir
+    --output_dir ./outputs/debug
