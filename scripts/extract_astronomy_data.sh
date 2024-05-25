d43ae739b04a4c38ca6bf0a6ab7c67a738508b96
Maxwell-Jia <mr.minghui.jia@gmail.com>
2024-05-25 20:12:42 +0800

    extract data for more format

--- scripts/extract_astronomy_data.sh
+++ scripts/extract_astronomy_data.sh
@@ -8,8 +8,9 @@ OUTPUT_DIR=${INPUT_DIR}/datasets
 DATASETS_DIR=/mnt/geogpt-gpfs/llm-course/public/datasets
 
 python ${INPUT_DIR}/src/exp/extract_astronomy_data.py \
-    --astro_title_path ${INPUT_DIR}/datasets/astro-title.txt \
-    --document_dir ${DATASETS_DIR}/dolma_v1_7/CC_head/documents \
-    --similar_threshold 0.8 \
-    --output_path ${OUTPUT_DIR}/astro_text.jsonl \
-    --max_items 1000
+    --examples_path ${INPUT_DIR}/datasets/astro-wiki-examples.jsonl \
+    --documents_dir ${DATASETS_DIR}/wikipedia/20231101.en/ \
+    --output_path ${OUTPUT_DIR}/wiki-astro.jsonl \
+    --max_items 5 \
+    --gpu 7 \
+    > ${OUTPUT_DIR}/extract_astro.log 2>&1
