a2acc8b604da959fdbc80db06391a9949512aab0
Maxwell-Jia <mr.minghui.jia@gmail.com>
2024-06-18 12:12:11 +0800

    clearn project files

--- llama3.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export HF_ENDPOINT=https://hf-mirror.com
-export HF_HOME=/mnt/geogpt-gpfs/llm-course/home/jiaminghui/huggingface/
-
-source /mnt/geogpt-gpfs/llm-course/home/jiaminghui/Miniconda3/bin/activate
-conda activate llm
-
-python llama3.py
